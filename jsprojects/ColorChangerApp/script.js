abf1e4bd098d7b27b2da004b441a06ab93ee3613
sureshpallapu <pallapusuresh2000@gmail.com>
2025-06-18 22:33:13 +0530

    Add Digital Clock project inside jsprojects

--- jsprojects/ColorChangerApp/script.js
+++ jsprojects/ColorChangerApp/script.js
@@ -1,1 +1,1 @@
-"function changeColor() { document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16); }" 
+function changeColor() { document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16); } 
